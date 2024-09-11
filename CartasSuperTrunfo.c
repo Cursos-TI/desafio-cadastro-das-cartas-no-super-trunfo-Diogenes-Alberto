7a354363b282a8298522545debf54e70008f5741
Diogenes-Alberto <74563187+Diogenes-Alberto@users.noreply.github.com>
2024-09-11 00:25:20 +0000

    alterando tipo do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -16,18 +17,17 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    int  codigoCidadeA01  ,codigoCidadeA02  ,codigoCidadeA03  ,codigoCidadeA04;
-    char nomeCidadeA01[20],nomeCidadeA02[20],nomeCidadeA03[20],nomeCidadeA04[20];
-    int  populacaoCidadeA01  ,populacaoCidadeA02  ,populacaoCidadeA03  ,populacaoCidadeA04;
-    float  areaCidadeA01  ,areaCidadeA02  ,areaCidadeA03  ,areaCidadeA04;
-    float  pibCidadeA01  ,pibCidadeA02  ,pibCidadeA03  ,pibCidadeA04;
-    int  pointsCidadeA01  ,pointsCidadeA02  ,pointsCidadeA03  ,pointsCidadeA04;
+    int codigo=1;
+    char  codigoCidadeA01[3]  ,codigoCidadeA02[3]  /*,codigoCidadeA03[3]  ,codigoCidadeA04[3]*/;
+    char nomeCidadeA01[20],nomeCidadeA02[20]/*,nomeCidadeA03[20],nomeCidadeA04[20]*/;
+    int  populacaoCidadeA01  ,populacaoCidadeA02  /*,populacaoCidadeA03  ,populacaoCidadeA04*/;
+    float  areaCidadeA01  ,areaCidadeA02  /*,areaCidadeA03  ,areaCidadeA04*/;
+    float  pibCidadeA01  ,pibCidadeA02  /*,pibCidadeA03  ,pibCidadeA04*/;
+    int  pointsCidadeA01  ,pointsCidadeA02  /*,pointsCidadeA03  ,pointsCidadeA04*/;
 
     printf("Digite o nome da Cidade 01:\n");
-    scanf(" %S",&nomeCidadeA01);
-    printf("Digite o codigo da Cidade 01:\n");
-    scanf("%d",&codigoCidadeA01);
+    scanf(" %s",nomeCidadeA01);
+    
     printf("Digite a populacao da Cidade 01:\n");
     scanf("%d",&populacaoCidadeA01);
     printf("Digite a area da Cidade 01:\n");
@@ -38,9 +38,7 @@ int main() {
     scanf("%d",&pointsCidadeA01); 
 
     // printf("Digite o nome da Cidade 02:\n");
-    // scanf(" %S",&nomeCidadeA02);
-    // printf("Digite o codigo da Cidade 02:\n");
-    // scanf("%d",&codigoCidadeA02);
+    // scanf(" %s",nomeCidadeA02);
     // printf("Digite a populacao da Cidade 02:\n");
     // scanf("%d",&populacaoCidadeA02);
     // printf("Digite a area da Cidade 02:\n");
@@ -50,15 +48,15 @@ int main() {
     // printf("Digite a quantidade de pontos turisticos da Cidade 02:\n");
     // scanf("%d",&pointsCidadeA02); 
 
-    printf("Nome Cidade 01: %S\n",nomeCidadeA01);
-    printf("Codigo da Cidade 01: %d\n",codigoCidadeA01);
+    printf("Nome Cidade 01: %s\n",nomeCidadeA01);
+    //printf("Codigo da Cidade 01: %d\n",codigoCidadeA01);
     printf("populacao da Cidade 01: %d\n",populacaoCidadeA01);
     printf("Area da Cidade 01: %.2f\n",areaCidadeA01);
     printf("Pib Cidade 01: %.2f\n",pibCidadeA01);
     printf("Pontos Turisticos da Cidade 01: %d\n",pointsCidadeA01);
 
-    // printf("Nome Cidade 02: %S\n",nomeCidadeA02);
-    // printf("Codigo da Cidade 02: %d\n",codigoCidadeA02);
+    // printf("Nome Cidade 02: %s\n",nomeCidadeA02);
+    // //printf("Codigo da Cidade 02: %d\n",codigoCidadeA02);
     // printf("populacao da Cidade 02: %d\n",populacaoCidadeA02);
     // printf("Area da Cidade 02: %.2f\n",areaCidadeA02);
     // printf("Pib Cidade 02: %.2f\n",pibCidadeA02);
