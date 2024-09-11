ef4b033f696a4fb6831f55851e757f1cb2d5e66d
Diogenes-Alberto <74563187+Diogenes-Alberto@users.noreply.github.com>
2024-09-11 02:09:00 +0000

    Adicionando Propriedades de densidade e pibper capita e alterando a forma de exibiçao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,39 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 
+void retornaIdCidade ( char nome[], char texto[], int indice)
+{
+    char convertido[5];
+    sprintf(convertido,"%d",indice);
+    strcpy(nome,texto);
+    strcat(nome,convertido);
+}
+float DensidadePopulacional(int populacao, float areaCidade)
+{
+    float resultado;
+    resultado=populacao/areaCidade;
+    return resultado;
+}
+float PibPerCapita(int populacao, float pibCidade)
+{
+    float resultado;
+    resultado=pibCidade/populacao;
+    return resultado;
+}
+
+void MostrarDados(char numeroCidade[],char nomeCidade[],char codigoCidade[],int populacaoCidade,int pointsCidade,
+                    float areaCidade,float pibCidade,float densidadePopulacional, float pibPerCapita)
+{
+    printf("Nome Cidade %s: %s\n",numeroCidade,nomeCidade);
+    printf("Codigo da Cidade %s: %s \n",numeroCidade,codigoCidade);
+    printf("populacao da Cidade %s: %d\n",numeroCidade,populacaoCidade);
+    printf("Area da Cidade %s: %.2f\n",numeroCidade,areaCidade);
+    printf("Pib Cidade %s: %.2f\n",numeroCidade,pibCidade);
+    printf("Pontos Turisticos da Cidade %s: %d\n",numeroCidade,pointsCidade);
+    printf("Densidade Populacional da Cidade %s: %.2f\n",numeroCidade,densidadePopulacional);
+    printf("Pib per Capita da Cidade %s: %.2f\n",numeroCidade,pibPerCapita);
+}
+
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -18,16 +51,16 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     int codigo=1;
-    char  codigoCidadeA01[3]  ,codigoCidadeA02[3]  /*,codigoCidadeA03[3]  ,codigoCidadeA04[3]*/;
-    char nomeCidadeA01[20],nomeCidadeA02[20]/*,nomeCidadeA03[20],nomeCidadeA04[20]*/;
-    int  populacaoCidadeA01  ,populacaoCidadeA02  /*,populacaoCidadeA03  ,populacaoCidadeA04*/;
-    float  areaCidadeA01  ,areaCidadeA02  /*,areaCidadeA03  ,areaCidadeA04*/;
-    float  pibCidadeA01  ,pibCidadeA02  /*,pibCidadeA03  ,pibCidadeA04*/;
-    int  pointsCidadeA01  ,pointsCidadeA02  /*,pointsCidadeA03  ,pointsCidadeA04*/;
+    char  codigoCidadeA01[4]  ;
+    int  populacaoCidadeA01,pointsCidadeA01 ;
+    float  areaCidadeA01,densidadePopulacionalA01,pibPerCapitaA01 ;
+    float  pibCidadeA01  ;
+    char nomeCidadeA01[20];
 
+    retornaIdCidade(codigoCidadeA01,"A0",codigo);
+    
     printf("Digite o nome da Cidade 01:\n");
     scanf(" %s",nomeCidadeA01);
-    
     printf("Digite a populacao da Cidade 01:\n");
     scanf("%d",&populacaoCidadeA01);
     printf("Digite a area da Cidade 01:\n");
@@ -35,32 +68,11 @@ int main() {
     printf("Digite o pib da Cidade 01:\n");
     scanf("%f",&pibCidadeA01);
     printf("Digite a quantidade de pontos turisticos da Cidade 01:\n");
-    scanf("%d",&pointsCidadeA01); 
+    scanf("%d",&pointsCidadeA01);
 
-    // printf("Digite o nome da Cidade 02:\n");
-    // scanf(" %s",nomeCidadeA02);
-    // printf("Digite a populacao da Cidade 02:\n");
-    // scanf("%d",&populacaoCidadeA02);
-    // printf("Digite a area da Cidade 02:\n");
-    // scanf("%f",&areaCidadeA02);
-    // printf("Digite o pib da Cidade 02:\n");
-    // scanf("%f",&pibCidadeA02);
-    // printf("Digite a quantidade de pontos turisticos da Cidade 02:\n");
-    // scanf("%d",&pointsCidadeA02); 
+    densidadePopulacionalA01=DensidadePopulacional(populacaoCidadeA01,areaCidadeA01);
+    pibPerCapitaA01=PibPerCapita(populacaoCidadeA01,pibCidadeA01);
 
-    printf("Nome Cidade 01: %s\n",nomeCidadeA01);
-    //printf("Codigo da Cidade 01: %d\n",codigoCidadeA01);
-    printf("populacao da Cidade 01: %d\n",populacaoCidadeA01);
-    printf("Area da Cidade 01: %.2f\n",areaCidadeA01);
-    printf("Pib Cidade 01: %.2f\n",pibCidadeA01);
-    printf("Pontos Turisticos da Cidade 01: %d\n",pointsCidadeA01);
-
-    // printf("Nome Cidade 02: %s\n",nomeCidadeA02);
-    // //printf("Codigo da Cidade 02: %d\n",codigoCidadeA02);
-    // printf("populacao da Cidade 02: %d\n",populacaoCidadeA02);
-    // printf("Area da Cidade 02: %.2f\n",areaCidadeA02);
-    // printf("Pib Cidade 02: %.2f\n",pibCidadeA02);
-    // printf("Pontos Turisticos da Cidade 02: %d\n",pointsCidadeA02);
-
+    MostrarDados("01",nomeCidadeA01,codigoCidadeA01,populacaoCidadeA01,pointsCidadeA01,areaCidadeA01,pibCidadeA01,densidadePopulacionalA01,pibPerCapitaA01);
     return 0;
 }
